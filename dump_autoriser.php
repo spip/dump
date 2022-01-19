56a5ebe500ae208b6162cb3c7af1316f6f228829
JamesRezo <james@rezo.net>
2022-01-19 23:44:25 +0100

    fix(cs): utf-8 sans BOM et copyright a priori

--- dump_autoriser.php
+++ dump_autoriser.php
@@ -1,14 +1,14 @@
-﻿<?php
+<?php
 
-/***************************************************************************\
+/***************************************************************************
  *  SPIP, Système de publication pour l'internet                           *
  *                                                                         *
- *  Copyright © avec tendresse depuis 2001                                 *
+ *  Copyright (c) avec tendresse depuis 2001                               *
  *  Arnaud Martin, Antoine Pitrou, Philippe Rivière, Emmanuel Saint-James  *
  *                                                                         *
  *  Ce programme est un logiciel libre distribué sous licence GNU/GPL.     *
  *  Pour plus de détails voir le fichier COPYING.txt ou l'aide en ligne.   *
-\***************************************************************************/
+ ***************************************************************************/
 
 /**
  * Déclarations d'autorisations
